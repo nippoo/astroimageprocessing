687a96fe5553f74f87c4da25bd019ac4929b3357
Max Hunter <nippoo@nippoo.net>
2014-03-10 11:23:14 +0000

    Processed data and created mask

--- loaddata.py
+++ loaddata.py
@@ -3,5 +3,6 @@ import pyfits       # PyFITS at https://pythonhosted.org/pyfits
 
 hdudata = pyfits.open('mosaic.fits') # we hardcode the input data file
 img = hdudata[0].data # img is a NumPy array with the data
+mask = np.ones(img.shape, dtype='bool') # create mask with the same dimensions. We set the bit low to mask it
 
-print img.shape
+print img
