262a46587f0282c3fc9c1485dd002940e5fcebdb
Max Hunter <nippoo@nippoo.net>
2014-03-13 15:39:02 +0000

    Refactored code to have modular data loader

--- stardetect.py
+++ stardetect.py
@@ -5,3 +5,6 @@ class StarDetect:
         # Finds the brightest unmasked pixel in an image and returns a tuple with its coordinates.
         maskedimg = image * mask
         return np.unravel_index(np.argmax(maskedimg), image.shape)
+        
+    def MaskStar(image, coords, size=[[12, 12]]):
+        # Masks star based on highest 
