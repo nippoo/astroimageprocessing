0897f8bdaa77a8a0c57dff4431af358d27a8b2a3
Max Hunter <nippoo@nippoo.net>
2014-03-13 16:00:10 +0000

    Refactored code

--- stardetect.py
+++ stardetect.py
@@ -1,10 +1,18 @@
 import numpy as np
+from starprocessor import StarProcessor
 
-class StarDetect:
-    def FindBrightest(image, mask):
-        # Finds the brightest unmasked pixel in an image and returns a tuple with its coordinates.
-        maskedimg = image * mask
-        return np.unravel_index(np.argmax(maskedimg), image.shape)
-        
-    def MaskStar(image, coords, size=[[12, 12]]):
-        # Masks star based on highest 
+s = StarProcessor()
+
+# StarFinder.OpenFile(s)
+
+def FindBrightest(image, mask):
+    # Finds the brightest unmasked pixel in an image and returns a tuple with its coordinates.
+    maskedimg = image * mask
+    return np.unravel_index(np.argmax(maskedimg), image.shape)
+    
+def MaskStar(image, coords, size=[[12, 12]]):
+    pass
+    # Masks star based on highest 
+    
+print FindBrightest(s.img, s.mask)
+print np.argmax(s.img)
--- starprocessor.py
+++ starprocessor.py
@@ -5,14 +5,13 @@ import matplotlib.pyplot as plt
 # Easier to hardcode the file, since we're working with a single file for the moment
 filename = 'mosaic.fits'
 
-# Class for loading and saving data
-class StarFinder:
+class StarProcessor:
+    def __init__(self):
+        self.OpenFile()
+        
     def OpenFile(self):    
         self.hdudata = pyfits.open(filename) # we hardcode the input data file
         self.img = self.hdudata[0].data # img is a NumPy array with the data
         self.mask = np.ones(self.img.shape, dtype='bool') # create mask with the same dimensions. We set the bit low to mask it
         
         self.masked = self.img
-        
-    def __init__(self):
-        self.OpenFile()
