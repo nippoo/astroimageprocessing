341b4d44938e5606fd09aef0d7dfca7560875c96
nexus314 <snb11@imperial.ac.uk>
2014-03-13 16:55:59 +0000

    Masks a circle centred on a given coord

--- test code for masking.py
+++ test code for masking.py
@@ -1,18 +1,14 @@
 import numpy as np
 from starprocessor import StarProcessor
-
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.path as path
 
-
-
 s = StarProcessor()
 
 
-
+s.MaskStar([50,50])
 
 plt.clf()
-plt.imshow(s.img)
+plt.imshow(s.mask)
 plt.show()
-
