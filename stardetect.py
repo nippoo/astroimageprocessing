4e2a3cf24de0de0f2a0c9fd276f90afed15c0c73
nexus314 <snb11@imperial.ac.uk>
2014-03-14 10:32:49 +0000

    flux working

--- stardetect.py
+++ stardetect.py
@@ -8,7 +8,7 @@ stars = []
 for x in range(0, 10):
     starloc = s.FindBrightest()
     if s.MaskStar(starloc): # this is a new star, add it to our dict
-		flux = s.Flux(starloc)	#calculates the Flux from the count for the star
-        stars.append({'coords':starloc, 'count':s.img[starloc], 'flux':flux})	#adds star's paramters to catalogue
+		flux = s.flux(starloc)	#calculates the Flux from the count for the star
+		stars.append({'coords':starloc, 'count':s.img[starloc], 'flux':flux})	#adds star's paramters to catalogue
          
 print stars
