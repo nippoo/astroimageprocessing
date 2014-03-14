f7278e9359a53b36cc9456567f8a9bd046cd54d7
Max Hunter <nippoo@nippoo.net>
2014-03-14 10:22:54 +0000

    Fixed bug in stardetect

--- stardetect.py
+++ stardetect.py
@@ -8,6 +8,6 @@ stars = []
 for x in range(0, 10):
     starloc = s.FindBrightest()
     if s.MaskStar(starloc): # this is a new star, add it to our dict
-         stars.append({'coords':starloc, 'mag':s.img(starloc)})
+         stars.append({'coords':starloc, 'mag':s.img[starloc]})
          
 print stars
