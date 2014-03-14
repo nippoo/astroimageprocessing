611dab84d18ed42615556b19a53f13b58495c7cf
Max Hunter <nippoo@nippoo.net>
2014-03-14 10:21:48 +0000

    Added dict to contain detected star information

--- stardetect.py
+++ stardetect.py
@@ -3,4 +3,11 @@ from starprocessor import StarProcessor
 
 s = StarProcessor()
 
-print s.FindBrightest()
+stars = []
+
+for x in range(0, 10):
+    starloc = s.FindBrightest()
+    if s.MaskStar(starloc): # this is a new star, add it to our dict
+         stars.append({'coords':starloc, 'mag':s.img(starloc)})
+         
+print stars
