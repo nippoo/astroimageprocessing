b08f081fe88311e01c2fe3d6dfe70a5c92909d5b
nexus314 <snb11@imperial.ac.uk>
2014-03-14 11:29:02 +0000

    fixed star numbering

--- stardetect.py
+++ stardetect.py
@@ -8,15 +8,14 @@ import matplotlib.path as path
 s = StarProcessor()
 
 stars = []
-
-for x in range(0, 500):
-    
+x=0
+while x<500:
     starloc = s.FindBrightest()
     if s.MaskStar(starloc): # this is a new star, add it to our dict
         flux = s.flux(starloc)	#calculates the Flux from the count for the star
         stars.append({'coords':starloc, 'count':s.img[starloc], 'flux':flux})	#adds star's paramters to catalogue
         print "Star", x, "found with count", s.img[starloc]
-        
+        x=x+1
     if s.img[starloc] < 4540: # we say this isn't a star any more - 2sd above background
         break
         
