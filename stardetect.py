acef67d487ada9203901fcacec786237841ee77c
nexus314 <snb11@imperial.ac.uk>
2014-03-17 15:36:48 +0000

    changed to work with flux not counts

--- stardetect.py
+++ stardetect.py
@@ -9,17 +9,15 @@ s = StarProcessor()
 
 stars = []
 x=0
-while x<2000:
+while x<1000:
     starloc = s.FindBrightest()
     if s.MaskStar(starloc): # this is a new star, add it to our dict
-        flux = s.flux(starloc)	#calculates the Flux from the count for the star
-        stars.append({'coords':starloc, 'count':s.img[starloc], 'flux':flux})	#adds star's paramters to catalogue
-        print "Star", x, "found with count", s.img[starloc]
+        stars.append({'coords':starloc, 'flux':s.img[starloc]})	#adds star's paramters to catalogue
+        print "Star", x, "found with flux", s.img[starloc]
         x=x+1
     if s.img[starloc] < 3500: # we say this isn't a star any more - 2sd above background
         break
         
-countlist = [i['count'] for i in stars]
 fluxlist = [i['flux'] for i in stars]
 values, base = np.histogram(fluxlist, bins=40)
 cumulative = np.cumsum(values)
