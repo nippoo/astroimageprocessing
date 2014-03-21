9f02aef52d9025ae48943242664f964606d184be
nexus314 <snb11@imperial.ac.uk>
2014-03-21 11:42:27 +0000

    removed unnecessary plotting

--- galaxydetect.py
+++ galaxydetect.py
@@ -13,32 +13,23 @@ s = StarProcessor()
 s.mask = np.load("maskfile.npy")	#load the premask
 s.RecalculateMasked
 
-plt.clf()
-plt.imshow(s.mask)
-plt.show()
-
-plt.clf()
-plt.imshow(s.masked)
-plt.show()
-
 stars = [] # intialise catalogue
 
 #galaxy detection
 x = 0
-while x<1000:
-	print x
+while x<500:
+	
 	starloc = s.FindBrightest()
 	if s.img[starloc]<3500:
 		print "hit background level in count"
 		break
 	galcount=s.MaskGalaxy(starloc)
-	stars.append({'coords':starloc, 'ave count':galcount})	#adds star's paramters to catalogue
+	stars.append({'coords':starloc, 'count':galcount})	#adds star's paramters to catalogue
+	print x, "	", galcount
 	x=x+1
 	
-print stars
+#print stars
 
-
-np.load("maskfile", s.mask)
 
 plt.clf()
 plt.imshow(s.mask)
@@ -51,7 +42,7 @@ plt.show()
 
 
 s.RecalculateMasked
-fluxlist = [i['ave count'] for i in stars]
+fluxlist = [i['count'] for i in stars]
 values, base = np.histogram(fluxlist, bins=40)
 cumulative = np.cumsum(values)
 plt.plot(base[:-1], cumulative, c='blue')
