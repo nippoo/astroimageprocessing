9638526945bd98c0cc134ef3a4bcc4aa552d87c9
nexus314 <snb11@imperial.ac.uk>
2014-03-24 16:07:32 +0000

    error bars

--- cummplotter.py
+++ cummplotter.py
@@ -16,9 +16,11 @@ print stars
 fluxlist = [i['flux'] for i in stars]
 values, base = np.histogram(fluxlist, bins=40)
 cumulative = np.cumsum(values)
+x_error = ((s.header['MAGZRR'])**2+(2.5*(4000**-1)*(np.log(10)**-1)*500)**2)**0.5
+print x_error
 y_error = cumulative**0.5
 print cumulative
 print y_error
-plt.errorbar(base[:-1], cumulative, y_error, c='blue')
+plt.errorbar(base[:-1], cumulative,y_error, x_error, c='blue')
 #plt.semilogy()
 plt.show()
