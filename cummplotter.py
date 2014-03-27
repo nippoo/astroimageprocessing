1ce3f805ca68f69c267e63ff208386eb8cc5fc4e
nexus314 <snb11@imperial.ac.uk>
2014-03-27 16:20:11 +0000

    x-error still in progress

--- cummplotter.py
+++ cummplotter.py
@@ -14,10 +14,10 @@ stars = np.load("catalogue.npy")
 #print stars
 
 fluxlist = [i['flux'] for i in stars]
-errorlist = [i['fluxerror'] for i in stars]
-b = 10          #specifies the number of bins for the historgram
+#errorlist = [i['fluxerror'] for i in stars]
+b = 80          #specifies the number of bins for the historgram
 values, base = np.histogram(fluxlist, bins=b)
-flxerrors, baseerror = np.histogram(fluxlist, bins=b)
+#flxerrors, baseerror = np.histogram(fluxlist, bins=b)
 cumulative = np.cumsum(values)
 
 
@@ -27,6 +27,6 @@ y_error = cumulative**0.5
 
 #print cumulative
 #print y_error
-plt.errorbar(base[:-1], cumulative,y_error, x_error, c='blue')
+plt.errorbar(base[:-1], cumulative,y_error, c='blue')
 plt.semilogy()
 plt.show()
