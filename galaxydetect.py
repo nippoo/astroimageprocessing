41bfccd264ad9aa7a3ccc895f271d25181babc7e
nexus314 <snb11@imperial.ac.uk>
2014-03-27 16:19:53 +0000

    outputting to csv file

--- galaxydetect.py
+++ galaxydetect.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.path as path
 
+import csv
+
 # finds galaxies and stores the corresponding data in the catalogue.
 
 s = StarProcessor()
@@ -24,16 +26,24 @@ while x<1000:
     	print "hit background level in count"
     	break
     galradius=s.FindGalaxyRadius(starloc)
+    #galradius = 12
     galcount, galerror=s.MaskGalaxy(starloc, Gradius=galradius, inner_Bradius = galradius + 15, Bradius = galradius + 80)
     if galcount>0:
 		galflux=s.count_to_flux(galcount)
 		galfluxerror=s.count_to_flux_error(galcount,galerror)
-		stars.append({'coords':starloc, 'count':galcount, 'flux':galflux, 'fluxerror':galfluxerror})	#adds star's paramters to catalogue
+		stars.append({'coords':starloc, 'peakcount':s.img[starloc],'avecount':galcount, 'flux':galflux, 'fluxerror':galfluxerror, 'radius':galradius})	#adds star's paramters to catalogue
 		print x,"	", galcount, "	", galflux,  "	", galradius, "	", galfluxerror
 		x=x+1
 #print stars
 
-
+s.RecalculateMasked
+np.save("catalogue", stars)
+with open('starcatalogue.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['Average Count', 'Average Flux', 'Error in Flux',])
+    for i in stars:
+        spamwriter.writerow([i['coords'][0], i['coords'][1],i['peakcount'],i['avecount'], i['flux'], i['fluxerror'], i['radius']])
+        
 plt.clf()
 plt.imshow(s.mask)
 plt.show()
@@ -41,11 +51,3 @@ plt.show()
 plt.clf()
 plt.imshow(s.masked)
 plt.show()
-
-s.RecalculateMasked
-np.save("catalogue", stars)
-#with open('starcatalogue.csv', 'wb') as csvfile:
- #   spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-  #  spamwriter.writerow(['header1', 'header2'])
-   # for i in stars:
-    #    spamwriter.writerow([i['count'], i['flux'], i['fluxerror'])
